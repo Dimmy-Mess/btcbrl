ba4d03490d26da3b076f8d116e5fbf352514625b
Demitry Messias Santos <dimmy.messias@gmail.com>
2021-05-12 01:00:26 -0300

    Deploy attempt 1

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
 import tweepy
-import credentials
+#import credentials >> For testing!
 import msg
 import time
+
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
 auth.set_access_token(credentials.access_token,credentials.access_token_secret)
