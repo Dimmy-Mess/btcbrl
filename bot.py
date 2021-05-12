26255783d65a8ad8b7db231d7925092002f82013
Demitry Messias Santos <dimmy.messias@gmail.com>
2021-05-12 00:48:43 -0300

    corrected tweeting time

--- bot.py
+++ bot.py
@@ -7,13 +7,16 @@ auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret
 auth.set_access_token(credentials.access_token,credentials.access_token_secret)
 api = tweepy.API(auth)
 
+hours = 12
+tweeting_time = 3600*hours
+
 
 def Tweeting():
 
     while True:
         
         api.update_status(msg.composer())
-        time.sleep(3600)
+        time.sleep(tweeting_time)
 
 
 
